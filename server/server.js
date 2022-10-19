f4ac400a2b4939bed43bc80c1a336272d45b7c17
rva-mike <mike.levy.rva@gmail.com>
2022-10-19 16:02:18 -0400

    comments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//requirements
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
@@ -7,6 +8,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
+//whatever is in the environment variable PORT, or 3000 if there's nothing there.
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -24,6 +26,7 @@ const startServer = async () => {
 
 startServer();
 
+//express middleware (functions or methods in expressJS for carrying out various operations on requests made to the server.)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -32,6 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+//mongo db connection
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
