62cfd3fea1efccd6bcef46b9399def2e816b0359
rva-mike <mike.levy.rva@gmail.com>
2022-10-19 15:32:12 -0400

    reworked server.js- start server and apollo middleware

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
 const express = require("express");
-// import ApolloServer
-const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require("./utils/auth");
-
 const path = require("path");
 const db = require("./config/connection");
 
-// const routes = require("./routes");
-
-// import our typeDefs and resolvers
+// add apollo server
+const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+// add apollo middleware
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,19 +32,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-// app.use(routes);
-
 db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-db.on("error", (err) => {
-  console.error("MongoDB connection error: ", err);
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
